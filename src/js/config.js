d6ed6de7687f7637cfbcb4bb6badd76187f02e26
Sleyter De La Cruz <Sleyter2616@gmail.com>
2021-01-28 20:28:16 -0500

    remove old file

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `a1df706a-a09b-4065-bcd7-f50c4c9f0ba7`;
+export const MODAL_CLOSE_SEC = 2.5;
